65985c8896b333bbfe3bbaca94dd101e048910d3
Sudheer Salavadi <connect.uxmaster@gmail.com>
2022-11-17 21:40:43 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gbfirst`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
